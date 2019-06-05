68a1dd582ae9a125f4be97e6f754ccf2a77d440a
Vitoe Alberto de Souza <39542138+vitoralberto@users.noreply.github.com>
2019-06-05 11:34:19 -0300

    Add files via upload

---
+++ semaforo/Processo.java
@@ -0,0 +1,13 @@
+package Semaforo;
+public class Processo {
+
+    int tipo;
+    int idprocesso;
+
+    
+    public Processo(int idprocesso, int tipo) {
+        this.tipo = tipo;
+        this.idprocesso = idprocesso;
+
+    }
+}
